0e1ab8ff4fd32b978d51ae59ea7668c77af2766a
Rendy Isdwanta <93455508+rendyisd@users.noreply.github.com>
2025-02-13 17:49:03 +0700

    feat: telescope keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,9 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = 'Telescope help tags' })
+
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
